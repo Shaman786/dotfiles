15d20dbf6ddf0b65ebb7a128c59b999b4ffd2a91
unknown <Fayaj Hossain>
2025-12-15 17:17:19 +0530

    chore: updated the existing keymaps in a more cleaner fashion

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,31 +1,47 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    lazy = false, -- IMPORTANT on Windows
-    -- Defining 'config' overrides Lazy.nvim's default behavior of calling setup()
+    lazy = false,
     config = function()
-      -- 1. Load your separate configuration file manually
       require("config.dap")
-
-      -- OR (Recommended) Put the config logic directly here to keep it self-contained
-      -- local dap = require("dap")
-      -- dap.adapters["pwa-node"] = ...
     end,
+    -- Move keymaps here
+    keys = {
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Debug: Toggle Breakpoint",
+      },
+      {
+        "<leader>dc",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Debug: Continue / Start",
+      },
+      {
+        "<leader>do",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Debug: Step Over",
+      },
+      {
+        "<leader>di",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Debug: Step Into",
+      },
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle()
+        end,
+        desc = "Debug: Toggle UI",
+      },
+    },
   },
-  -- Keymaps for nvim-dap
-  vim.keymap.set("n", "<leader>db", function()
-    require("dap").toggle_breakpoint()
-  end, { desc = "Debug: Toggle Breakpoint" }),
-  vim.keymap.set("n", "<leader>dc", function()
-    require("dap").continue()
-  end, { desc = "Debug: Continue / Start" }),
-  vim.keymap.set("n", "<leader>do", function()
-    require("dap").step_over()
-  end, { desc = "Debug: Step Over" }),
-  vim.keymap.set("n", "<leader>di", function()
-    require("dap").step_into()
-  end, { desc = "Debug: Step Into" }),
-  vim.keymap.set("n", "<leader>du", function()
-    require("dapui").toggle()
-  end, { desc = "Debug: Toggle UI" }),
 }
