104fa1e2b5630748277a3a951ab7efd0cc1f7f30
unknown <Fayaj Hossain>
2025-12-15 16:13:24 +0530

    fix: trying to patch in dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -2,6 +2,14 @@ return {
   {
     "mfussenegger/nvim-dap",
     lazy = false, -- IMPORTANT on Windows
-    config = false,
+    -- Defining 'config' overrides Lazy.nvim's default behavior of calling setup()
+    config = function()
+      -- 1. Load your separate configuration file manually
+      require("config.dap")
+
+      -- OR (Recommended) Put the config logic directly here to keep it self-contained
+      -- local dap = require("dap")
+      -- dap.adapters["pwa-node"] = ...
+    end,
   },
 }
